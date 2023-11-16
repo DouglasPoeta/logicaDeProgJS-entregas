b0448cec23588d84a87b62bc8d1db3b14715fae9
Douglas Poeta <dpoeta75@gmail.com>
2023-11-16 20:15:59 -0300

    Commit de atualizacao

--- atividade 4/scripts.js
+++ atividade 4/scripts.js
@@ -4,15 +4,15 @@ let alturaNaoFormatada = prompt('Qual sua altura?');
 
 let altura = parseFloat(alturaNaoFormatada.replace(',', '.'));
   
-IMC = peso / (altura * altura);
+const IMC = peso / (altura * altura);
 
 if(IMC < 18.5){
     console.log("Abaixo do peso");
 
-}else if(IMC >= 18.5 && IMC < 24.9  ){
+}else if(IMC >= 18.5 && IMC <= 24.99  ){
     console.log("Peso normal");
 
-}else if(IMC >= 25 && IMC < 29.9){
+}else if(IMC >= 25 && IMC < 29.99){
     console.log("Sobrepeso"); 
     
 }else {
